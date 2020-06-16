754c2aa97ed7c16ff88caabb1087563ae6cb55a6
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-06-15 18:48:38 -0600

    Added skelleton manifest.

---
+++ Package.swift
@@ -0,0 +1,13 @@
+// swift-tools-version:5.2
+
+import PackageDescription
+
+let package = Package(
+  name: "MP4v2",
+  products: [
+    .library(name: "MP4v2", targets: ["CMP4v2"])
+  ],
+  targets: [
+    .target(name: "CMP4v2")
+  ]
+)
