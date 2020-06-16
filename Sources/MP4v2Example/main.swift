9829f065293a01b5848c25abc5eb19ba1a7e0664
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-06-15 23:31:04 -0600

    Implemented a little sample.

--- Sources/MP4v2Example/main.swift
+++ Sources/MP4v2Example/main.swift
@@ -1,3 +1,12 @@
+import Foundation
 import CMP4v2
 
-print(MP4FileHandle.self)
+let fileURL = URL(fileURLWithPath: NSHomeDirectory())
+  .appendingPathComponent("Desktop")
+  .appendingPathComponent("MP4 File.mp4")
+
+print("Creating an empty MP4 at “\(fileURL.path)”...")
+let file = MP4Create(fileURL.path.cString(using: .utf8), 0)
+defer { MP4Close(file, 0) }
+
+print("All done.")
